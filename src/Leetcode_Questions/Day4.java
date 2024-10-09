c17efda4f835a7a4392f97ace49650e1b5b8f71b
Tushar k <tushar.kale@mitcorer.edu.in>
2024-10-09 23:14:23 +0530

    Day4 :  - 79.word Search Leetcode Question.

---
+++ src/Leetcode_Questions/Day4.java
@@ -0,0 +1,73 @@
+package Leetcode_Questions;
+
+/*
+
+79. word Search
+
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Example 1:
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+
+ */
+import  java.util.*;
+public class Day4 {
+    private int rows;
+    private int cols;
+    private Set<String> visited;
+
+    public boolean exist(char[][] board, String word) {
+        rows = board.length;
+        cols = board[0].length;
+        visited = new HashSet<>();
+
+        Map<Character, Integer> count = new HashMap<>();
+        for (char c : word.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        if (count.getOrDefault(word.charAt(0), 0) > count.getOrDefault(word.charAt(word.length() - 1), 0)) {
+            word = new StringBuilder(word).reverse().toString();
+        }
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (dfs(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int r, int c, int k) {
+        if (k == word.length()) {
+            return true;
+        }
+
+        if (r < 0 || r >= rows || c < 0 || c >= cols || visited.contains(r + "," + c) || board[r][c] != word.charAt(k)) {
+            return false;
+        }
+
+        visited.add(r + "," + c);
+        boolean res = dfs(board, word, r + 1, c, k + 1) ||
+                dfs(board, word, r - 1, c, k + 1) ||
+                dfs(board, word, r, c + 1, k + 1) ||
+                dfs(board, word, r, c - 1, k + 1);
+        visited.remove(r + "," + c);
+        return res;
+    }
+}
