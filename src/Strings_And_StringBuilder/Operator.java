7ae6ad4815dbd9908d25cd7465089b16fc3c7acb
Tushar k <tushar.kale@mitcorer.edu.in>
2024-10-12 22:39:42 +0530

    String and StringBuilder

---
+++ src/Strings_And_StringBuilder/Operator.java
@@ -0,0 +1,14 @@
+package Strings_And_StringBuilder;
+
+public class Operator {
+    public static void main(String[] args) {
+        System.out.println('a'+'b');
+
+        System.out.println("a"+"b");
+
+        System.out.println((char)('a'+3));
+
+        System.out.println("a"+1);
+        // integer will be converted to Integer that will toString()
+    }
+}
---
+++ src/Strings_And_StringBuilder/PrettyPrinting.java
@@ -0,0 +1,12 @@
+package Strings_And_StringBuilder;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float num = 0.44534F;
+
+
+        System.out.printf("Formatted num is %.2f", num);
+        System.out.println( );
+        System.out.printf("PI values %.3f", Math.PI);
+    }
+}
