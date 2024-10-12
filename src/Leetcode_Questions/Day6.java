a20ceb30e901b29baaff89659a6c89ae5eccb348
Tushar k <tushar.kale@mitcorer.edu.in>
2024-10-12 22:40:08 +0530

    Leetcode Day6

--- src/Leetcode_Questions/Day6.java
+++ src/Leetcode_Questions/Day6.java
@@ -31,22 +31,16 @@ public class Day6 {
     public List<List<Integer>> fourSum(int[] arr, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         int len = arr.length;
-
         Arrays.sort(arr);
-
         for (int i = 0; i < len - 3; i++) {
             if (i > 0 && arr[i - 1] == arr[i]) continue;
-
             for (int j = i + 1; j < len - 2; j++) {
                 if (j > i + 1 && arr[j - 1] == arr[j]) continue;
-
                 int k = j + 1;
                 int l = len - 1;
-
                 while (k < l) {
                     long sum = arr[i] + arr[j];
                     sum += arr[k] + arr[l];
-
                     if (sum == target) {
                         ans.add(Arrays.asList(arr[i], arr[j], arr[k], arr[l]));
                         k++;
