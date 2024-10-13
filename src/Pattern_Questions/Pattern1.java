6f59ff05f71c4719a038fa4fe739e1216121895b
Tushar k <tushar.kale@mitcorer.edu.in>
2024-10-13 23:17:18 +0530

    starting  solving  pattern  questions.

---
+++ src/Pattern_Questions/Pattern1.java
@@ -0,0 +1,26 @@
+package Pattern_Questions;
+
+/*
+below is patten we have to solve
+*
+* *
+* * *
+* * * *
+
+ */
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        pattern1(4 );
+    }
+    static  void pattern1(int n){
+        for(int row = 1;row<=n;row++){
+            // for every row ,run the col
+            for(int col=1;col<=row;col++){
+                System.out.print(" * ");
+            }
+            // when one row is printed , we need to  add a newline.
+            System.out.println();
+        }
+    }
+}
