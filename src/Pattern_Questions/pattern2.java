0cb126abdbc096cb1a47301212b15fe1706a6044
Tushar k <tushar.kale@mitcorer.edu.in>
2024-10-28 10:48:36 +0530

    Patten2 of java

---
+++ src/Pattern_Questions/pattern2.java
@@ -0,0 +1,19 @@
+package Pattern_Questions;
+
+public class pattern2 {
+    public static void main(String[] args) {
+        pattern2(4);
+        
+    }
+    static  void pattern2(int n){
+        for(int row = 1;row<=n;row++){
+            // for every row ,run the col
+            for(int col=1;col<=n;col++){
+                System.out.print(" * ");
+            }
+            // when one row is printed , we need to  add a newline.
+            System.out.println();
+        }
+    }
+
+}
