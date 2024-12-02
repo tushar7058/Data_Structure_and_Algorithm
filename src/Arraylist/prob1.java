7f642eae605daa9711104ed856884aad8eb3cd1f
Tushar k <tushar.kale@mitcorer.edu.in>
2024-12-02 21:56:46 +0530

    arraylist

---
+++ src/Arraylist/prob1.java
@@ -0,0 +1,25 @@
+package Arraylist;
+
+import java.util.ArrayList;
+
+public class prob1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(); // declare arrayList
+
+        list.add(12);
+        list.add(13);
+        list.add(14);
+
+        list.add(0,11);
+        System.out.println(list);
+
+        for(int i =0;i< list.size();i++){
+            System.out.println(list.get(i));
+            break;
+        }
+        for(Integer i:list){
+            System.out.println("new list through for each loop:"+ i);
+            break;
+        }
+    }
+}
