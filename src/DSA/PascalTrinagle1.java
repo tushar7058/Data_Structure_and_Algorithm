f36c9b6349557e5048b5126b29f90f45b1f81e33
Tushar k <tushar.kale@mitcorer.edu.in>
2024-12-06 16:07:19 +0530

    PascalTriangle  3 Variation

---
+++ src/DSA/PascalTrinagle1.java
@@ -0,0 +1,25 @@
+package DSA;
+
+
+import java.util.*;
+
+public class PascalTrinagle1 {
+    // Variation 2
+    static void pascalTriangle(int n) {
+        long ans = 1;
+        System.out.print(ans + " "); // printing 1st element
+
+        // Printing the rest of the part:
+        for (int i = 1; i < n; i++) {
+            ans = ans * (n - i);
+            ans = ans / i;
+            System.out.print(ans + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        pascalTriangle(n);
+    }
+}
