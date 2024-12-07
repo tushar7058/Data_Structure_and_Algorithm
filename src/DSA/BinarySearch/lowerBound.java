0446d0df028438ed124f07453f3e31f79d7250ef
Tushar k <tushar.kale@mitcorer.edu.in>
2024-12-07 15:18:58 +0530

    lowerBound

---
+++ src/DSA/BinarySearch/lowerBound.java
@@ -0,0 +1,22 @@
+package DSA.BinarySearch;
+
+public class lowerBound {
+    public static int lowerBound(int []arr, int n, int x) {
+        // Write your code here
+        int low =0;
+        int high = arr.length-1;
+        int ans = n;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>=x){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans ;
+    }
+}
