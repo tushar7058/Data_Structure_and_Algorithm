307b214c59fff60c569de97152bb49c2af4c34a8
Tushar k <tushar.kale@mitcorer.edu.in>
2024-12-13 13:24:29 +0530

    Find the Middle Element of the LinkedList

---
+++ src/DSA/LinkedList/MiddleElement.java
@@ -0,0 +1,59 @@
+package DSA.LinkedList;
+
+
+import java.util.*;
+
+// Node class represents a node in a linked list
+class Node4{
+    // Data stored in the node
+    int data;
+    // Pointer to the next node in the list
+    Node next;
+
+    // Constructor with both data
+    // and next node as parameters
+    Node4(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    // Constructor with only data as
+    // a parameter, sets next to null
+    Node4(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class MiddleElement {
+    static Node findMiddle(Node head) {
+        // Initialize the slow pointer to the head.
+        Node slow = head;
+        // Initialize the fast pointer to the head.
+        Node fast = head;
+        // Traverse the linked list using
+        // the Tortoise and Hare algorithm.
+        while (fast != null && fast.next != null && slow != null) {
+            // Move fast two steps.
+            fast = fast.next.next;
+            // Move slow one step.
+            slow = slow.next;
+        }
+        // Return the slow pointer,
+        // which is now at the middle node.
+        return slow;
+    }
+    public static void main(String[] args) {
+        // Creating a sample linked list:
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        // Find the middle node
+        Node middleNode = findMiddle(head);
+        // Display the value of the middle node
+        System.out.println(STR."The middle node value is: \{middleNode.data}");
+    }
+}
+
+
