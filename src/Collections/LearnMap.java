50510b635c891bb9c833df235c263af545753efe
Tushar k <tushar.kale@mitcorer.edu.in>
2024-12-20 22:48:34 +0530

    Hashmap in java

---
+++ src/Collections/LearnMap.java
@@ -0,0 +1,29 @@
+package Collections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+public class LearnMap{
+    public static void main(String[] args) {
+
+        // Map<String,Integer> mpp = new HashMap<>();
+        Map<String,Integer> mpp = new TreeMap<>();
+
+        mpp.put("one", 1);
+        mpp.put("two",2);
+        mpp.put("three",3);
+
+        System.out.println("map key value pair is:"+mpp);
+
+        //iteration in mapp
+
+        for(Map.Entry<String,Integer>e:mpp.entrySet()){
+         
+
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+
+    
+        }    
+    }
+}
