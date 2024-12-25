a823a5b0f8b95517873707f1e7eb2ccecdeae3a2
Tushar k <tushar.kale@mitcorer.edu.in>
2024-12-25 13:08:10 +0530

    Next Greater element 2 || in this prob we can able to look circular array

---
+++ src/DSA/StackQueue/NextGreaterElement2.java
@@ -0,0 +1,20 @@
+package DSA.StackQueue;
+import java.util.*;
+class NextGreaterElement2 {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length; 
+        int nge [] = new int[n]; // creating n size arrr  to save ans
+        Stack<Integer>st = new Stack<>(); // creating stack
+
+        for(int i = 2*n-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=nums[i%n]){
+                st.pop(); // to delete ele
+            }
+            if(i<n){
+                nge[i] = st.isEmpty()? -1 : st.peek(); // ternary operations varName = conditon ? if True :  if false
+            }
+            st.push(nums[i%n]);
+        }
+        return nge;
+    }
+}
