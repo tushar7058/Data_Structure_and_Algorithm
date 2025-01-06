b3872359f00ce77826d17a739eb443e73887374a
Tushar k <115905283+tushar7058@users.noreply.github.com>
2025-01-06 21:45:19 +0530

    Fibonacci Num Using Recursion  | recursion | basics

---
+++ src/Collections/FibonacciNthNo.java
@@ -0,0 +1,14 @@
+package Collections;
+
+public class FibonacciNthNo {
+    public static void main(String[] args) {
+        System.out.println("fibonacci num is :"+fibo(0 ));
+    }
+    static int  fibo(int n ){
+        //base condition 
+        if(n<2){
+            return n ;
+        }   
+        return fibo(n-1)+fibo(n-2);
+    }
+}
