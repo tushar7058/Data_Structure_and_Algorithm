8d1ae6a4d1130b4aed7a1a0d7119efc30bc4db72
tushar7058 <tusharkale816@gmail.com>
2025-01-21 16:23:11 +0530

    reverse updated

--- src/DSA/Recursion/reverse1.java
+++ src/DSA/Recursion/reverse1.java
@@ -3,12 +3,11 @@ package DSA.Recursion;
 
 public class reverse1 {
     public static void main(String[] args) {
-        reverseNo(1234);
-        System.out.println(sum);
+        // reverseNo(1234);
+        System.out.println(reverse2(1234));
     }
     static int sum = 0;
     static void reverseNo(int n ){
-        // base conditon
         if(n==0){
             return ;
         }
@@ -17,4 +16,24 @@ public class reverse1 {
 
         reverseNo(n/10);  
     }    
+    // second way to reverse a function.
+    static int  reverse2(int n){
+        // sometimes might you need some additional variables in the arguments
+        // in that case , make another function
+
+        int digit = (int)(Math.log10(n))+1;
+        return helper(n,digit); 
+    }
+    // helper function for calculatio
+    private static int helper(int n ,int digit){
+        // base condition
+        if(n%10 == n){
+            return 1;
+        }
+        // calculate remainder
+        int rem = n%10;
+        // returning the value
+        return rem* (int)(Math.pow(10,digit-1)) +helper(n/10, digit-1);
+    }
 }
+
