63432d30d3347b8ba82f227c597d18c607aa41ea
tushar7058 <tusharkale816@gmail.com>
2025-02-04 15:11:26 +0530

    Printing Linearly num | Basic Recursion problem

---
+++ src/DSA/Recursion/StriverPlaylist/printLinearlyNum.java
@@ -0,0 +1,15 @@
+package DSA.Recursion.StriverPlaylist;
+
+public class printLinearlyNum {
+    public static void main(String[] args) {
+        PrintLinearly(0, 5);
+    }
+    static void PrintLinearly(int i,int n){
+        // base conditoion
+        if(i>=n){
+            return;
+        }
+        System.out.println(i);
+        PrintLinearly(i+1, n);
+    }
+}
