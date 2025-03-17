2e7d37dee876a20e54f20281170df92da96609b9
tushar7058 <tusharkale816@gmail.com>
2025-03-17 16:40:56 +0530

    Revision of String

--- src/Strings_And_StringBuilder/palindrome.java
+++ src/Strings_And_StringBuilder/palindrome.java
@@ -19,4 +19,18 @@ public class palindrome {
         }
         return true;
     }
+
+    static boolean isvalid(String s){
+        s = s.toLowerCase();
+        for(int i =0;i<=s.length();i++){
+            char start = s.charAt(i);
+            char end = s.charAt(s.length()-1-i);
+
+
+            if(start!=end){
+                return false;
+            }
+        }
+        return true;
+    }
 }
