ea0f4913696def4d99114de71f974199bc1412fc
tushar7058 <tusharkale816@gmail.com>
2025-03-25 12:52:33 +0530

    logic building

---
+++ src/Basics/LogicBuilding/Addition.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Addition{
+
+	public static void main(String[]args){
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("enter a input of a :");
+			int   a= sc.nextInt();
+			System.out.println("enter a input of b :");
+			int   b = sc.nextInt();
+
+			System.out.println("The Result of this :"+additionOfTwoNo(a,b));
+
+	}
+	static int additionOfTwoNo(int   a, int b){
+		return  a +  b;
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
