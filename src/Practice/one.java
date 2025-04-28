dc8feaf3022896b6d0fbae25a52776592dba734e
tushar7058 <kaletushar991@gmail.com>
2025-04-28 21:40:31 +0530

    Restart

---
+++ src/Practice/one.java
@@ -0,0 +1,10 @@
+public class one{
+
+    public static void main(String[]args){
+        hello();
+        System.out.println("Hello world");
+    }
+    static int  hello(){
+        return 0;
+    }
+} 
