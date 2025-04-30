3183ae751cb04a5cb910f3c765e1d1ff53a9590b
Tusharkale0912 <Tushar.kale@phntechnology.com>
2025-04-30 06:33:48 +0530

    Optimized Solution | Day2 | Upskilling

--- src/DSA/Array/LongestSubarray.java
+++ src/DSA/Array/LongestSubarray.java
@@ -3,8 +3,10 @@ class LongestSubarray{
     public static void main(String[]args){
 
         int [] arr = {1,2,3,1,1,1,4,2,3};
-        int target = 6;
-        findlongest(arr,target);
+        // int target = 6;
+        int k = 6;
+        // findlongest(arr,target);
+       System.out.println( "longest length of subarray sum :"+findlongest1(arr,k));
     }
 
     // Brute 
@@ -27,4 +29,27 @@ class LongestSubarray{
        }
        System.out.println("the length of longest subarray sum "+ target + " is : "+ maxlength );
     }
+
+    /*
+        - Two pointer Approach
+        - Optimized Solution
+    */
+    static int findlongest1(int[]arr,int k){
+        int left = 0 , right =0;
+        int sum =0 , maxlen = 0;
+        int n = arr.length;
+
+        while(right<n){
+            sum += arr[right];
+            while(sum>k && left<=right){
+                sum-=arr[left];
+                left++;
+            }
+            if(sum==k){
+                maxlen = Math.max(maxlen,right-left+1);
+            }
+            right++;
+        }
+        return maxlen;
+    }   
 }
